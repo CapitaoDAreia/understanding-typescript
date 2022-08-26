764bee8ab171a36ba03515777c15ac40432c1a5c
Igor Silva <danieligord7@hotmail.com>
2022-08-26 09:19:44 -0300

    SECTION 3: Compiler and its settings

--- 2 - Compilador/compilador.js
+++ 2 - Compilador/compilador.js
@@ -1,18 +1,16 @@
 //TSCONFIG.JSON - noEmitOnError:  Se qualquer erro for reportado, nenhum arquivo JS será gerado
-let canal: string = 'Gaveta';
-let inscritos: number = 987789
+var canal = 'Gaveta';
+var inscritos = 987789;
 // canal = inscritos
-
 //TSCONFIG.JSON - target: essa propriedade diz qual é a versão do JS que será utilizado no arquivo compilado
 //se você mudar de es2016 para es3, por exemplo, os recursos de 2016 não serão utilizados
-class funcao{
-    nome:string
-    sobrenome:string
-    constructor(nome: string, sobrenome: string){
-        this.nome= nome,
-        this.sobrenome=sobrenome
+var funcao = /** @class */ (function () {
+    function funcao(nome, sobrenome) {
+        this.nome = nome,
+            this.sobrenome = sobrenome;
     }
-    falaNome(): string{
-        return this.nome+""+this.sobrenome
-    }
-}
+    funcao.prototype.falaNome = function () {
+        return this.nome + "" + this.sobrenome;
+    };
+    return funcao;
+}());
