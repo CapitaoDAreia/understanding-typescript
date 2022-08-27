6edd2aa6dfd6d74f66aca49d54af2806cde67779
Igor Silva <danieligord7@hotmail.com>
2022-08-27 10:34:34 -0300

    SECTION 3: Done with types and some tips about the compiler

--- 2 - Compilador/compilador.ts
+++ 2 - Compilador/compilador.ts
@@ -13,6 +13,30 @@ class funcao{
         this.sobrenome=sobrenome
     }
     falaNome(): string{
+        console.log("Teste")
         return this.nome+""+this.sobrenome
     }
 }
+
+
+/*
+TSCONFIG.JSON - sourceMap: Permite que arquivos JS emitidos possuam um mapa para serem analisados no ambiente de produção
+                *strictNullChecks: Impede que nulos e indefinidos sejam compilados
+                *noUsedParameters: Impede que parâmetros não utilizados passem na compilação
+                *noUsedLocals: Impede que variáveis não utilizadas passem na copilação
+
+                outDir: determina o destino dos arquivos compilados
+                outFile: determina que os arquivos buildados saiam em um só arquivo
+*/
+
+function saudar(isManha: boolean, /*unusedPar: boolean*/):string {
+    let saudacao: string;
+    // let unusedLocal: string;
+    if(isManha){
+        saudacao = "Bom dia!"
+    }else{
+        saudacao = "Tenha uma boa vida!"
+    }
+    return "Saudacao: "+ saudacao;
+}
+
