5d75416c8529e9b390706bf6aa276837369f1436
Igor Silva <danieligord7@hotmail.com>
2022-08-27 11:53:27 -0300

    SECTION 4: Understanding Typescript in variable types and functions

--- 3 - Typescript and ECMAScript Resources/ecmascript.ts
+++ 3 - Typescript and ECMAScript Resources/ecmascript.ts
@@ -5,32 +5,37 @@
     Const - Não é possível realizar atribuição em uma variável const, também possui escopo de bloco assim como let.
 */
 // console.log(seraquepode)
-var seraquepode = "?";
-var estaFrio = true;
-if (estaFrio) {
-    var acao = "Colocar casado!";
+let seraquepode = "?"
+let estaFrio = true;
+if(estaFrio){
+    var acao = "Colocar casado!"
 }
 // console.log(acao)
+
+
 /*
     ARROW FUNCTION
     Sintaxe - Exemplo abaixo. Pode possuir retorno implícito e se o parâmetro for tipado é necessário o
-    This -
+    This - 
 */
-var falarCom = function (pessoa) {
+const falarCom = (pessoa: string):string =>{
     // console.log(this)
-    return "Eaê, " + pessoa;
-};
-var falarCom2 = function (pessoa) { return "Eaê, " + pessoa; };
+    return "Eaê, "+pessoa
+}
+const falarCom2 = (pessoa:string) => "Eaê, "+pessoa
+
 //This - Percea que em uma função com sintaxe padrão o this pode ser alterado com o método bind.
 //       Por padrão, uma função recebe como this o mesmo this do contexto em que foi declarada.  
-function exemploThis1() {
+function exemploThis1(){
     // console.log(this)
 }
-exemploThis1();
-var exemploThis2 = exemploThis1.bind('This Alterado');
-exemploThis2();
-var falarCom3 = falarCom.bind("Ihaaaaaa");
-falarCom('Igor');
-falarCom3('Igor');
+exemploThis1()
+const exemploThis2 = exemploThis1.bind('This Alterado')
+exemploThis2()
+
+const falarCom3 = falarCom.bind("Ihaaaaaa")
+falarCom('Igor')
+falarCom3('Igor')
 //Percea agora que em uma arrowFunction o this recebido funciona da mesma forma que na função padrão, com 
 //a diferença que o this de uma arrow é imutável
+ 
