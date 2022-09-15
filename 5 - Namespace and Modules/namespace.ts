0749019de67d98c55f0f7ded0a2245c4556f0b55
Igor <danieligord7@hotmail.com>
2022-09-14 21:55:02 -0300

    SECTION 6: Intro to namespace

--- 5 - Namespace and Modules/namespace.ts
+++ 5 - Namespace and Modules/namespace.ts
@@ -1,1 +1,22 @@
-/*STARTING...*/
+/*
+    NAMESPACE
+
+    É uma funcionalidade do TS utilizada para agrupar funções, variáveis, interfaces, classes e etc
+    que possuem algum tipo de correlação.
+    Para usar os componentes de um namespace fora dele é necessário realizar a exportação.
+*/
+
+namespace NamesPace1 {
+    export function Funcao1(): string{
+        return `Função 1 da namespace1 invocada.`
+    }
+    function Funcao2(): string{
+        return `Função 2 da namespace1 invocada.`
+    }
+    export function Funcao3():string{
+        return `Função 3 da namespace1:${Funcao2()}`
+    }
+};
+
+console.log(NamesPace1.Funcao1());
+console.log(NamesPace1.Funcao3());
