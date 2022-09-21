4a5e51dde492969795e2c3f9fef5e095ed33e790
Igor <danieligord7@hotmail.com>
2022-09-20 23:10:54 -0300

    SECTION 9: Starting to understand decorators...

--- 8 - Decorator/decorator.js
+++ 8 - Decorator/decorator.js
@@ -1,14 +1,8 @@
 /*
     DECORATORS
 
-    Estou começando a entender o que são decorators.
     São espécies de funções que ajudam a "decorar"/modificar o comportamento de classes, métodos e propriedades.
 
-    A função que constrói o MethodDecorator leva três argumentos padrões:
-        -target: método/função/classe a ser decorado
-        -key: o nome do método/função/classe que está sendo decorado
-        -value: o descritor da propriedade/função/classe dada se existir no objeto, ou será undefined caso não exista. O descritor da propriedade/função/classe é obtido baseado na função
-        Object.getOwnPropertyDescriptor()
 */
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -16,53 +10,91 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-const decoratorEletrodomesticos = (target) => console.log(target); //Função usada como decorator que imprime o parâmetro recebido. Esse parâmetro é a classe que foi decorada
-let Eletrodomesticos = class Eletrodomesticos {
-    constructor(tipo) {
-        this.tipo = tipo;
-        this.tipo = 'Eletrico';
-    }
-    falaTipo() {
-        console.log(this.tipo);
+/*
+    PRIMEIRO EXEMPLO - DECORATOR DE CLASSES SIMPLES
+*/
+let MinhaClasse = class MinhaClasse {
+    constructor(nome = 'Minha Classe') {
+        this.nome = nome;
+        console.log(`Construindo ${this.nome}...`);
     }
 };
-Eletrodomesticos = __decorate([
-    decoratorEletrodomesticos //Usando decorator na classe, associando a classe à função decorator
-], Eletrodomesticos);
-//--------------------------------------------------------------------------------------------------
+MinhaClasse = __decorate([
+    recebeClasseELoga
+], MinhaClasse);
+const umaClasse = new MinhaClasse();
+function recebeClasseELoga(target) {
+    console.log(target);
+}
 /*
-    TESTES
+    O QUE FOI FEITO:
+    Criamos uma classe e instanciamos a mesma e a anotamos/decoramos com uma função (decoratorMethod).
+    Essa função decoratorMethod recebe a classe anotada/decorada como parâmetro.
+    Logamos essa classe anotada/decorada para entender que o decoratorMethod recebe ela.
 */
-function log(prefix) {
-    return (target) => {
-        console.log(`${prefix} ${target}`);
-        console.log(target);
+/*
+    SEGUNDO EXEMPLO - DECORATOR FACTORY, UMA FUNÇÃO QUE RETORNA UM DECORATOR
+*/
+let MinhaSegundaClasse = class MinhaSegundaClasse {
+    constructor(nome) {
+        this.nome = nome;
+        console.log(`Construindo a classe ${this.nome}`);
+        this.nome = 'Sou a segunda classe.';
+    }
+};
+MinhaSegundaClasse = __decorate([
+    decoratorCondicional(true)
+], MinhaSegundaClasse);
+function decoratorVazio(_) { }
+function decoratorDeClasse(target) {
+    console.log(`Classe recebida para decorar: ${target}`);
+}
+function decoratorCondicional(validacao) {
+    return validacao ? decoratorDeClasse : decoratorVazio;
+}
+/*
+    O QUE FOI FEITO:
+    Criamos uma classe MinhaSegundaClasse sem instanciar
+    Criamos um decoratorMethod condicional que, dado um resultado, retorna um decorator vazio ou um decorator de classe. Neste caso, retornamos um decorator vazio pois um decoratorMethod não pode retornar null ou undefined para o caso da variável validacao avaliar falso.
+
+    **Perceba que o decoratorVazio possui um parâmetro inutilizado, note a sintaxe.
+
+*/
+/*
+    TERCEIRO EXEMPLO - MODIFICANDO CONSTRUTOR USANDO DECORATOR DE CLASSES
+*/
+let MinhaTerceiraClasse = class MinhaTerceiraClasse {
+    constructor() {
+        console.log(`Construindo minha terceira classe...`);
+    }
+};
+MinhaTerceiraClasse = __decorate([
+    decoratorModificadorDeConstrutor //Assinando/decorando a terceira classe
+], MinhaTerceiraClasse);
+function decoratorModificadorDeConstrutor(target) {
+    console.log('Incidindo decorator sobre construtor da terceira classe...');
+    return class extends target {
+        constructor(...args) {
+            console.log('Invocando métodos da terceira classe...');
+            super(...args);
+            console.log('Adicionando propriedades além da terceira classe...');
+            //Aqui você pode adicionar mais à classe em questão
+            //Aqui você também pode assinar com outros tipos de decorators
+        }
     };
 }
-let Foo = class Foo {
-};
-Foo = __decorate([
-    log('Prefixo')
-], Foo);
-function handleDecorator(target, key) {
-    console.log(target, key);
-}
-class TestandoDecorators {
-    constructor(mainInfo) {
-        this.mainInfo = 'MaiInfo';
-    }
-}
-__decorate([
-    handleDecorator
-], TestandoDecorators.prototype, "mainInfo", void 0);
-function analyze(target, key) {
-    console.log(target, key);
-}
-class Task {
-    constructor(title) {
-        this.title = title;
-    }
-}
-__decorate([
-    analyze
-], Task.prototype, "title", void 0);
+new MinhaTerceiraClasse();
+new MinhaTerceiraClasse();
+new MinhaTerceiraClasse();
+/*
+    O QUE FOI FEITO:
+    Criamos uma terceira classe e logamos algo para identificar o seu constructor.
+
+    Após isso, criamos um tipo baseado em um construtor genérico, pois pode receber "qualquer" tipo de parâmetro em "qualquer" quantidade.
+
+    Definimos um decoratorMethod que recebe um parâmetro com a tipagem anteriormente criada, esse parâmetro é a nossa classe decorada.
+
+    Ao compilarmos o código, o decorator é inicializado e incide sobre a classe decorada retornando uma classe anônima que  estende a classe decorada. Esse decorator só é inicializado de fato uma vez, que é quando a classe assinada é inicializada.
+
+    Sempre que a classe decorada for instanciada, a classe anônima será retornada gerando uma herança da classe decorada estendendo-a.
+*/
--- 8 - Decorator/decorator.ts
+++ 8 - Decorator/decorator.ts
@@ -1,75 +1,103 @@
 /*
     DECORATORS
 
-    Estou começando a entender o que são decorators.
     São espécies de funções que ajudam a "decorar"/modificar o comportamento de classes, métodos e propriedades.
 
-    A função que constrói o MethodDecorator leva três argumentos padrões:
-        -target: método/função/classe a ser decorado
-        -key: o nome do método/função/classe que está sendo decorado
-        -value: o descritor da propriedade/função/classe dada se existir no objeto, ou será undefined caso não exista. O descritor da propriedade/função/classe é obtido baseado na função 
-        Object.getOwnPropertyDescriptor()
 */
 
-const decoratorEletrodomesticos=(target: Function)=>console.log(target); //Função usada como decorator que imprime o parâmetro recebido. Esse parâmetro é a classe que foi decorada
-
-@decoratorEletrodomesticos //Usando decorator na classe, associando a classe à função decorator
-class Eletrodomesticos {
-    constructor(public tipo: string){
-        this.tipo = 'Eletrico'
-    }
-
-    falaTipo(){
-        console.log(this.tipo)
-    }
-}
-
-
-
-
-//--------------------------------------------------------------------------------------------------
 /*
-    TESTES
+    PRIMEIRO EXEMPLO - DECORATOR DE CLASSES SIMPLES
 */
-function log(prefix: string){
-    return <Function>(target: Function)=>{
-        console.log(`${prefix} ${target}`);
-        console.log(target);
+@recebeClasseELoga
+class MinhaClasse{
+    constructor(public nome: string = 'Minha Classe'){
+        console.log(`Construindo ${this.nome}...`) 
+    }
+}
+const umaClasse = new MinhaClasse()
+
+function recebeClasseELoga(target: Function){
+    console.log(target) 
+}
+/*
+    O QUE FOI FEITO:
+    Criamos uma classe e instanciamos a mesma e a anotamos/decoramos com uma função (decoratorMethod).
+    Essa função decoratorMethod recebe a classe anotada/decorada como parâmetro.
+    Logamos essa classe anotada/decorada para entender que o decoratorMethod recebe ela.
+*/
+
+
+/*
+    SEGUNDO EXEMPLO - DECORATOR FACTORY, UMA FUNÇÃO QUE RETORNA UM DECORATOR
+*/
+
+@decoratorCondicional(true)
+class MinhaSegundaClasse{
+    constructor(public nome: string){
+        console.log(`Construindo a classe ${this.nome}`)
+        this.nome = 'Sou a segunda classe.'
     }
 }
 
-@log('Prefixo')
-class Foo{}
+function decoratorVazio(_: Function):void{}
 
-
-
-
-
-function handleDecorator(target: any, key: string) {
-    console.log(target, key);
+function decoratorDeClasse(target: Function){
+    console.log(`Classe recebida para decorar: ${target}`)
 }
 
+function decoratorCondicional(validacao: boolean){
+    return validacao ? decoratorDeClasse : decoratorVazio
+}
+/*
+    O QUE FOI FEITO:
+    Criamos uma classe MinhaSegundaClasse sem instanciar
+    Criamos um decoratorMethod condicional que, dado um resultado, retorna um decorator vazio ou um decorator de classe. Neste caso, retornamos um decorator vazio pois um decoratorMethod não pode retornar null ou undefined para o caso da variável validacao avaliar falso.
 
-class TestandoDecorators {
-    @handleDecorator
-    public mainInfo: string;
-    constructor(mainInfo: string){
-        this.mainInfo = 'MaiInfo'
+    **Perceba que o decoratorVazio possui um parâmetro inutilizado, note a sintaxe.
+
+*/
+
+
+/*
+    TERCEIRO EXEMPLO - MODIFICANDO CONSTRUTOR USANDO DECORATOR DE CLASSES
+*/
+@decoratorModificadorDeConstrutor //Assinando/decorando a terceira classe
+class MinhaTerceiraClasse{
+    constructor(){
+        console.log(`Construindo minha terceira classe...`)
     }
 }
+//Um tipo construtor que incide sobre uma função construtora genérica. Essa, por sua vez, atravez da expressão em questão (...args: any[]) recebe qualquer tipo de parâmetro.
+type constructor = {
+    new(...args: any[])
+}
 
-
-
-
-function analyze(target: any, key: string) {
-    console.log(target, key);
-  }
-  
-  class Task {
-    @analyze
-    public title: string;
-  
-    constructor(title: string) {
-      this.title = title;
+function decoratorModificadorDeConstrutor(target: constructor){ //target do tipo constructor
+    console.log('Incidindo decorator sobre construtor da terceira classe...')
+    return class extends target{ //retorna uma classe anônima que estende a classe decorada
+        constructor(...args: any[]){
+            console.log('Invocando métodos da terceira classe...')
+            super(...args)
+            console.log('Adicionando propriedades além da terceira classe...')
+            //Aqui você pode adicionar mais à classe em questão
+            //Aqui você também pode assinar com outros tipos de decorators
+            
+        }
     }
-  }
+}
+new MinhaTerceiraClasse();
+new MinhaTerceiraClasse();
+new MinhaTerceiraClasse();
+
+/*
+    O QUE FOI FEITO:
+    Criamos uma terceira classe e logamos algo para identificar o seu constructor.
+
+    Após isso, criamos um tipo baseado em um construtor genérico, pois pode receber "qualquer" tipo de parâmetro em "qualquer" quantidade.
+
+    Definimos um decoratorMethod que recebe um parâmetro com a tipagem anteriormente criada, esse parâmetro é a nossa classe decorada.
+
+    Ao compilarmos o código, o decorator é inicializado e incide sobre a classe decorada retornando uma classe anônima que  estende a classe decorada. Esse decorator só é inicializado de fato uma vez, que é quando a classe assinada é inicializada.
+
+    Sempre que a classe decorada for instanciada, a classe anônima será retornada gerando uma herança da classe decorada estendendo-a.
+*/
